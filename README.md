6d591fa4ff4d0aada3042604809c3632241f1650
ihza <ihzahmahendra6661@gmail.com>
2025-10-20 14:29:29 +0700

    initial

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularMinggu4
+# Angular232102585
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.3.2.
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
+import { Admin } from './admin/admin';
+import { Footer } from './footer/footer';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "dashboard", component: Dashboard },
+  { path: "admin", component: Admin },
+  { path: "login", component: Login },
+  { path: "signup", component: Signup },
+  { path: "footer", component: Footer }
+];
